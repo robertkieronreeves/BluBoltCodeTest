cd5ba2f7ec383ae9d3ceb6058792f455d59dbc19
Rob Reeves <robertkieronreeves@gmail.com>
2018-12-27 17:51:12 +0000

    added basic html structure

---
+++ ContactForm.php
@@ -0,0 +1,20 @@
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>BluBolt Contact Form</h3>
+    <h4>Please enter your details below:</h4>
+    <fieldset>
+      <input placeholder="Name:" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Email Address:" type="email" tabindex="2" required>
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your Message Here:" tabindex="3" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+ 
+  
+</div>
