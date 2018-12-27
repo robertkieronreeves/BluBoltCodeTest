6f84e0f28269db15bef3077e8403df15e26940c7
Rob Reeves <robertkieronreeves@gmail.com>
2018-12-27 18:10:15 +0000

    added basic style sheet

--- ContactForm.php
+++ ContactForm.php
@@ -1,15 +1,16 @@
+<link rel="stylesheet" href="Style.css" type="text/css"> 
 <div class="container">  
   <form id="contact" action="" method="post">
     <h3>BluBolt Contact Form</h3>
     <h4>Please enter your details below:</h4>
     <fieldset>
-      <input placeholder="Name:" type="text" tabindex="1" required autofocus>
+      <input placeholder="Name:" type="text" tabindex="1" autofocus>
     </fieldset>
     <fieldset>
-      <input placeholder="Email Address:" type="email" tabindex="2" required>
+      <input placeholder="Email Address:" type="text" tabindex="2">
     </fieldset>
     <fieldset>
-      <textarea placeholder="Type your Message Here:" tabindex="3" required></textarea>
+      <textarea placeholder="Type your Message Here:" type="text" tabindex="3" ></textarea>
     </fieldset>
     <fieldset>
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
